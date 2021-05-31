e4380dda56dc4f5071539a4b94f7e3b0d8fb8ada
Pzqqt <821026875@qq.com>
2021-05-31 10:11:22 +0800

    Optimize code style

--- kernel_org.py
+++ kernel_org.py
@@ -1,36 +1,38 @@
-#! /usr/bin/python3
-import requests as req
+#!/usr/bin/python3
+
 import time
 import logging
-from bs4.element import *
+
+import requests as req
 from bs4 import BeautifulSoup
+from bs4.element import Tag
+
 
 chat_id = "<your channel id>"
 bot_id = "<your bot id>"
 
-logging.basicConfig(level=logging.INFO,
-                    filename="<path to save logs>",
-                    filemode="w",
-                    format=
-                    "%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s"
-                    )
+logging.basicConfig(
+    level=logging.INFO,
+    filename="<path to save logs>",
+    filemode="w",
+    format="%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s"
+)
 
 def fetch_once():
     ret = []
     try:
         res = req.get("https://kernel.org")
-        bs = BeautifulSoup(res.text,features="html.parser")
+        bs = BeautifulSoup(res.text, features="html.parser")
         table = bs.find("table", id="releases")
         for row in table.children:
-            if type(row) != Tag:
+            if not isinstance(row, Tag):
                 continue
             ret.append(next(row.find_all("td")[1].strings))
         logging.info(ret)
-    except BaseException as e:
+    except Exception as e:
         logging.error(e)
-        return None
+        return
     return ret
-
 
 def list_cmp(list_old, list_new):
     return list(set(list_new) - set(list_old))
@@ -39,8 +41,10 @@ def send(what):
     status = 0
     while status != 200:
         logging.info("Try sending "+what)
-        status = req.post("https://api.telegram.org/bot" + bot_id + "/sendMessage", 
-           {"parse_mode":"Markdown", "text":what, "chat_id":chat_id}).status_code
+        status = req.post(
+                "https://api.telegram.org/bot%s/sendMessage" % bot_id,
+                {"parse_mode": "Markdown", "text": what, "chat_id": chat_id},
+            ).status_code
 
 last_result = fetch_once()
 while True:
@@ -49,5 +53,5 @@ while True:
     if next_result is None:
         continue
     for item in list_cmp(last_result, next_result):
-        send("*New Linux Kernel Release*\n["+item+"](https://www.kernel.org/)")
+        send("*New Linux Kernel Release*\n[%s](https://www.kernel.org/)" % item)
     last_result = next_result
